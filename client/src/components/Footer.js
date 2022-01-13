fb21e2a1bfe5e229e55ea20dc8339f68d7259817
Jakub Chrobak <jchrobakk@gmail.com>
2022-01-13 21:18:33 +0100

    add footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        created by <a href="#">jchrobakk</a> and <a href="">jkuryloo</a> -
+        devchallenges.io
+      </p>
+    </footer>
+  );
+}
