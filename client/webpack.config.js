c39c77c5a478762b714f91de3619c5e293c9000d
Jakub Chrobak <jchrobakk@gmail.com>
2022-01-15 14:17:57 +0100

    add file loader to webpack config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,6 +27,17 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[hash]-[name].[ext]",
+            },
+          },
+        ],
+      },
     ],
     // obecnie brak dodatkowych ustawień
   },
